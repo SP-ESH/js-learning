211e6cee6d75dc9fa0493c9dfe59ae8e43ffb9c5
Shubham Rawat <111427580+SP-ESH@users.noreply.github.com>
2023-10-07 03:13:06 +0000

    new files added

---
+++ 01_basics/variable.js
@@ -0,0 +1,6 @@
+const accountId = 3456
+let accountEmail = "Shubham@google.com"
+var accountPassword = "12345"
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountState])
